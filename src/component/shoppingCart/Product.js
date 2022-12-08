917d0c62d425ae7902e93d0932336f734b93a044
Lipitha <lipithapoojary123@gmail.com>
2022-12-08 12:56:08 +0530

    cart details

--- src/component/shoppingCart/Product.js
+++ src/component/shoppingCart/Product.js
@@ -1,11 +1,15 @@
-function Product({ product }) {
+function Product({ product,gatherCartInformation }) {
   // const { product } = props;
+  const addCart = (selectedProduct) => {
+    gatherCartInformation(selectedProduct);
+  };
+
   return (
     <div
       style={{
         border: "1px solid lightGreen",
         borderRadius: "7px",
-        marginBottom: "10px"
+        marginBottom: "10px",
       }}
     >
       <h3>{product.title}</h3>
@@ -14,18 +18,21 @@ function Product({ product }) {
         alt="productImage"
         style={{ height: "100px", width: "100px" }}
       />
-      <h3>Rs:{product.price}</h3>
+      <h3>${product.price}</h3>
       <p>{product.description}</p>
       <p>
         Ratings: {product.rating.rate}/5 ({product.rating.count})
       </p>
       <button
+        onClick={() => {
+          addCart(product);
+        }}
         style={{
           width: "40%",
           marginBottom: "5px",
           backgroundColor: "lightgreen",
           border: "none",
-          borderRadius: "6px"
+          borderRadius: "6px",
         }}
       >
         Add Cart
